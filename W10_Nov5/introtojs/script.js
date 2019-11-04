645d1d3c120f3089ffc57d358338ee1ed634d1ee
miraA <ma82@nyu.edu>
2019-11-04 17:43:23 -0500

    update nov 5

--- W10_Nov5/introtojs/script.js
+++ W10_Nov5/introtojs/script.js
@@ -1,135 +1,150 @@
-//send any messages to the developer's console
+//Simple JS to remember
 
+
+//send any messages to the developer's console. good for debugging!
 //console.log('Hellooooooooooooo!');
 
-//defining variables
 
-//something that stands for something else
-//sets a value to something
-//puts a name to a value
-//storing a value inside
 
-//a variable that contains a string
-var firstName = 'Jo'
-var lastName = ' Doe'
-//console.log(firstName)
-//console.log(lastName)
+// VARIABLES
+// Defining a variable with var + type of values: string
 
-//console.log(firstName[2]);
+//var firstName
 
-//string concatenation
-//console.log(firstName + lastName)
 
-//storing integers
-var num = 5
+//storing integers + floats
+//var num
 
-//storing float
-var num2 = 5.4
 
-//console.log(num + num2)
-// subtract -
-// divide /
-// multiply *
 
-//creating an array
+//console.log(num+num2);
 
-//holds a set
-//is a list
 
-//arrays in JS can have mixed data types
-var firstArray = ['bert', 'ernie', 'big bird', 'snuffie']
 
-//console.log(firstArray);
+
+
+
+
+// ARRAYS
+////var groceries = ['milk', 'eggs', 'bread', 'coffee'];
+
+//console.log(groceries);
 
 //arrays are indexed
-//console.log(firstArray[0]);
+//console.log(groceries[0]);
 
-var secondArray = ['kermit', 'miss piggy', 'gonzo', 'swedish chef']
+//var toDo = ['go to the store', 'send emails', 'call mom', 'feed the cat']
+
+//var stuffToDo = toDo.length; // array property
+//var moreStuff = groceries.join(" * "); //array method, it got those () > pop() removes last element, push() adds a new element
+
+
+//console.log(stuffToDo + ' ' + moreStuff);
 
 //multidimensional array
-var thirdArray = [firstArray, secondArray]
+//var multiArray= [groceries, toDo];
 
-//console.log(thirdArray[1][2]);
+//console.log(multiArray[0][2] + " and "+ multiArray[1][3]);
 
-//set up control flow
-//if and else statements
+//writing inside an HTML Element (we'll talk about DOM next class)
+//document.getElementById('here').innerText= multiArray[0][1];
 
-// <
-// >
-//<=
-//>=
-// ==
-// ===
 
-var num3 = 10
 
-//if (num3 === 14){
-//    console.log('your number is the correct number')
-//}else if{ (num > 10)
-//    console.log ('your number is too big')
-//}else{
-//    console.log('you did not choose a number')
-//}
 
-//functions are operations or methods
 
-function sayHello(){
-    console.log('helloooooooooooo')
+//LOOP THROUGH AN ARRAY
+/*var i;
+var text =" ";
+for (i = 0; i < groceries.length; i++) {
+  text += groceries[i] + " <br> ";
+}*/
+
+// check out the innerHTML vs innerText
+//document.getElementById('loopResult').innerHTML= text;
+
+//what are all the funny operators here: https://www.w3schools.com/js/js_operators.asp
+//more info on loops: https://www.w3schools.com/js/js_loop_for.asp
+
+
+
+
+
+
+// IF STATEMENTS
+// if too many groceries, I don't want to get them!
+
+
+/*if (groceries.length<=10){
+  document.getElementById('ifResult').innerText= "fine, i am going to the store";
+}else{
+  document.getElementById('ifResult').innerText= "i am staying home and eating chocolate";
+}*/
+
+
+
+
+
+
+// FUNCTIONS!
+// this is how you get stuff done!
+
+
+
+
+
+
+/*function myFunctionName(firstParameter, secondParamenter){
+  return(firstParameter+secondParamenter);
 }
 
-function addNumber(numberOne, numberTwo){
-    return numberOne + numberTwo //return exists the function
+var a = 6;
+var b = 7;
+
+document.getElementById('func_result').innerText=myFunctionName(a,b);
+*/
+
+
+
+
+
+// sometimes you need to execute a function, it's called invocation!
+
+
+
+
+/*
+function changeMyColor(){
+  document.getElementById('func_result').style.background="green";
 }
 
-//console.log(addNumber(40, 60));
-
-//create a function that is a virtual door
-//if the user goes through the tacos door, give them a message about delicious tacos
-//if the user goes through the pizza door, give them a message about yummy pizza
-//if the user goes through the noodles door, give them a message about awesome noodles
-
-//function that holds an if/else statement inside of it
-
-//you will test your doors by calling the function
-
-//function foodDoor(){
-//    if //goes through the tacos door
-//    //give message about delicious tacos
-//        //alert
-//        else if //goes through the pizza door
-//            //give them a message about yummy pizza
-//            else //if the user goes through the noodles door
-//                //give them a message about noodles
-//}
-
-//function foodDoor(door){
-//     //goes through the tacos door, give message about delicious tacos
-//    if (door ==='tacos'){
-//        console.log('Tacos are delicious')
-//    }else if( door === 'pizza'){//goes through the pizza door, give tjem a message about yummy pizza
-//        console.log('Pizza is yummy')
-//    } else if(door === 'noodle'){ // if the  user goes through the noodle door, give them message about awesome noodles
-//        console.log('Noodles are awesome')
-//    }else{
-//        console.log('please pick an appropriate door!')
-//    }
-//}
-//
-//foodDoor('mcdonalds')
+changeMyColor();
+*/
 
 
-function foodDoor(door) {
-	var output = 'Behind this door there is '
-	if (door === 1) {
-		output += 'Super Tacos'
-	} else if (door === 2) {
-		output += 'NY Pizzas'
-	} else if (door === 3) {
-		output += 'Ramen Noodles'
-	} else {
-		output += 'Nothing'
-	}
-	console.log(output)
-}
 
-foodDoor(3)
+//you can store function in a VARIABLES
+/*var mult = function (blah1, blah2) {return blah1 * blah2};
+document.getElementById("func_result2").innerHTML= mult(4, 100);*/
+
+
+
+// you can add time to your functions by setting Intervals
+//check out the text animation example
+
+// we want to cycle through our toDo list
+/*var loc=0;
+var duration=0;
+var addTime = setInterval(timeItOut, duration); // this gives it some timing, you say what function and how long
+
+
+function timeItOut(){
+
+  if (duration<300){
+    duration++;
+  }else{
+    document.getElementById('animate_it').innerText=toDo[loc];
+    clearInterval(addTime);
+  }
+
+}*/
---
+++ W10_Nov5/introtojs/script_filledOUT.js
@@ -0,0 +1,134 @@
+//Simple JS to remember
+
+
+//send any messages to the developer's console. good for debugging!
+//console.log('Hellooooooooooooo!');
+
+
+
+// VARIABLES
+// Defining a variable with var + type of values: string
+
+var firstName = 'Jo'
+var lastName = 'Doe'
+
+//storing integers + floats
+var num = 5;
+var num2 = 5.4;
+
+console.log(firstName+lastName + num);
+console.log(num+num2);
+
+//storing float
+//var num2 = 5.4;
+
+
+
+
+
+// ARRAYS
+var groceries = ['milk', 'eggs', 'bread', 'coffee'];
+
+console.log(groceries);
+
+//arrays are indexed
+console.log(groceries[0]);
+
+var toDo = ['go to the store', 'send emails', 'call mom', 'feed the cat']
+
+var stuffToDo = toDo.length; // array property
+var moreStuff = groceries.join(" * "); //array method, it got those () > pop() removes last element, push() adds a new element
+
+
+console.log(stuffToDo + ' ' + moreStuff);
+
+//multidimensional array
+var multiArray= [groceries, toDo];
+
+console.log(multiArray[0][2] + " and "+ multiArray[1][3]);
+
+//writing inside an HTML Element (we'll talk about DOM next class)
+document.getElementById('here').innerText= multiArray[0][1];
+
+
+
+
+
+//LOOP THROUGH AN ARRAY
+var i;
+var text =" ";
+for (i = 0; i < groceries.length; i++) {
+  text += groceries[i] + " <br> ";
+}
+
+// check out the innerHTML vs innerText
+document.getElementById('loopResult').innerHTML= text;
+
+//what are all the funny operators here: https://www.w3schools.com/js/js_operators.asp
+//more info on loops: https://www.w3schools.com/js/js_loop_for.asp
+
+
+
+
+
+
+// IF STATEMENTS
+// if too many groceries, I don't want to get them!
+if (groceries.length<=10){
+  document.getElementById('ifResult').innerText= "fine, i am going to the store";
+}else{
+  document.getElementById('ifResult').innerText= "i am staying home and eating chocolate";
+}
+
+
+
+
+
+
+// FUNCTIONS!
+// this is how you get stuff done!
+
+function myFunctionName(firstParameter, secondParamenter){
+  return(firstParameter+secondParamenter);
+}
+
+var a = 6;
+var b = 7;
+
+document.getElementById('func_result').innerText=myFunctionName(a,b);
+
+
+// sometimes you need to execute a function, it's called invocation!
+
+function changeMyColor(){
+  document.getElementById('func_result').style.background="green";
+}
+
+changeMyColor();
+
+
+//you can store function in a VARIABLES
+var mult = function (blah1, blah2) {return blah1 * blah2};
+document.getElementById("func_result2").innerHTML= mult(4, 100);
+
+
+
+// you can add time to your functions by setting Intervals
+//check out the text animation example
+
+// we want to cycle through our toDo list
+var loc=0;
+var duration=0;
+var addTime = setInterval(timeItOut, duration); // this gives it some timing, you say what function and how long
+
+
+function timeItOut(){
+
+  if (duration<300){
+    duration++;
+  }else{
+    document.getElementById('animate_it').innerText=toDo[loc];
+    clearInterval(addTime);
+  }
+
+}
